feb8d771942230619698abf5b4eae39769970cb7
Eugene Sosniuk <oliverhudson2304@gmail.com>
2020-12-11 23:18:48 +0200

    add

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+/*
+    3. Объявите две переменные: admin и name. Запишите в name строку "Василий". Замените значение из name в admin. Выведите admin.
+*/
+
+var name = prompt("Введите Ваше имя", '');
+var admin = name;
+
+alert('Имя администратора:' + admin);
+
+document.write ('Имя администратора:' + admin);   
+
